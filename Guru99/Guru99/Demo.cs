ef0b59158b01038aa2972b5898a03302054a7b0d
Yuriy Bryhas <yurikb@gmail.com>
2020-10-16 23:12:24 +0300

    added demo project

---
+++ Guru99/Guru99/Demo.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Guru99Demo
+{
+    class Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
